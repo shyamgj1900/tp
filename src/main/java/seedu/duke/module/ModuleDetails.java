fe735becd33cb0fc440e85e02751e6908787f639
saravanamani1999 <66838856+saravanamani1999@users.noreply.github.com>
2021-10-04 16:56:35 +0800

    add reading of json file and store it in array list

--- src/main/java/seedu/duke/module/ModuleDetails.java
+++ src/main/java/seedu/duke/module/ModuleDetails.java
@@ -1,5 +1,48 @@
 package seedu.duke.module;
 
 public class ModuleDetails {
-    
+
+    private String moduleCode;
+    private String title;
+    private String description;
+    private String department;
+    private String moduleCredit;
+    private String faculty;
+
+
+    public ModuleDetails(String moduleCode, String moduleCredit, String faculty,
+                         String description, String title, String department) {
+        this.moduleCode = moduleCode;
+        this.moduleCredit = moduleCredit;
+        this.faculty = faculty;
+        this.description = description;
+        this.title = title;
+        this.department = department;
+    }
+
+    public String getModuleCredit() {
+        return moduleCredit;
+    }
+
+    public String getModuleCode() {
+        return moduleCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+
 }
