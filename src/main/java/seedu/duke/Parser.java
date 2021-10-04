f7c8b5fa4f57f242243cf3f86be1af604f7e4fa8
Shyam <e0544484@u.nus.edu>
2021-10-04 15:37:06 +0800

    Add help message

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -2,6 +2,7 @@ package seedu.duke;
 
 import seedu.duke.commands.Command;
 import seedu.duke.commands.ExitCommand;
+import seedu.duke.commands.HelpCommand;
 import seedu.duke.commands.InvalidCommand;
 
 public class Parser {
@@ -10,6 +11,8 @@ public class Parser {
 
         String commandWord = input.split(" ")[0];
         switch (commandWord) {
+        case "help":
+            return new HelpCommand();
         case "bye":
             return new ExitCommand();
         default:
