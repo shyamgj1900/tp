3f84c7886d1b8dd138e208011b21cbcd1ca247af
NonRNP <69129667+NonRNP@users.noreply.github.com>
2021-10-04 15:47:55 +0800

    Implement cap calculator

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -1,5 +1,6 @@
 package seedu.duke;
 
+import seedu.duke.commands.CalculateCapCommand;
 import seedu.duke.commands.Command;
 import seedu.duke.commands.ExitCommand;
 import seedu.duke.commands.InvalidCommand;
@@ -10,6 +11,8 @@ public class Parser {
 
         String commandWord = input.split(" ")[0];
         switch (commandWord) {
+        case "cap":
+            return new CalculateCapCommand(input);
         case "bye":
             return new ExitCommand();
         default:
