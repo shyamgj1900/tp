40d4daf8d005663f5b84f1c687f7404d59bbb435
Shyam <e0544484@u.nus.edu>
2021-10-05 22:38:38 +0800

    Update help command

--- src/main/java/seedu/duke/commands/HelpCommand.java
+++ src/main/java/seedu/duke/commands/HelpCommand.java
@@ -5,8 +5,9 @@ public class HelpCommand extends Command {
     @Override
     public CommandResult executeCommand() {
         String helpMessage = "Here are the list of commands:\n"
-                + "cap - Opens the CAP calculator\n"
-                + "view [module code] - View the module details\n"
+                + "cap [MC and grade]  - Calculates the total cap for the semester\n"
+                + "view [enter module code] - View the module details\n"
+                + "bus routes - Check for a NUS bus route from stop to another\n"
                 + "info [su/modreg/plagiarism] - View information about SU, ModReg, or the Plagiarism Policy";
         return new CommandResult(helpMessage);
     }
