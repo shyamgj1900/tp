016824ae29b565f7b760c0a08b63b3228e7264ff
powzx <69313768+powzx@users.noreply.github.com>
2021-10-05 23:47:11 +0800

    Update HelpCommand

--- src/main/java/seedu/duke/commands/HelpCommand.java
+++ src/main/java/seedu/duke/commands/HelpCommand.java
@@ -5,10 +5,12 @@ public class HelpCommand extends Command {
     @Override
     public CommandResult executeCommand() {
         String helpMessage = "Here are the list of commands:\n"
-                + "cap [MC and grade]  - Calculates the total cap for the semester\n"
-                + "view [enter module code] - View the module details\n"
-                + "bus routes - Check for a NUS bus route from stop to another\n"
-                + "info [su/modreg/plagiarism] - View information about SU, ModReg, or the Plagiarism Policy";
+                + "1. cap MC_AND_GRADES  - Calculates the total cap for the semester\n"
+                + "2. view MODULE_CODE - View the module details\n"
+                + "3. bus routes - Check for a NUS bus route from stop to another\n"
+                + "4. planner add DESCRIPTION/DATE/START_TIME/END_TIME - Add an event to your schedule\n"
+                + "5. help - View this menu again\n"
+                + "6. bye - Exit Kolinux";
         return new CommandResult(helpMessage);
     }
 }
