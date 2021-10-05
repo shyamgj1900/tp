12a5e8716d251ac105b71b3df7e267b4f9b1c242
Shyam <e0544484@u.nus.edu>
2021-10-05 22:58:54 +0800

    Minor refactor

--- src/main/java/seedu/duke/routes/Graph.java
+++ src/main/java/seedu/duke/routes/Graph.java
@@ -5,13 +5,14 @@ import java.util.LinkedList;
 
 public class Graph {
     private int v;
-    private LinkedList<Integer> adj[];
+    private LinkedList<Integer>[] adj;
 
     public Graph(int v) {
         this.v = v;
         adj = new LinkedList[v];
-        for (int i = 0; i < v; i++)
+        for (int i = 0; i < v; i++) {
             adj[i] = new LinkedList<>();
+        }
     }
 
     public void addEdge(int v, int w) {
@@ -21,8 +22,8 @@ public class Graph {
     public Boolean isConnected(int u, int v) {
         int vertex;
         boolean[] visited = new boolean[this.v];
-        LinkedList<Integer> queue = new LinkedList<Integer>();
-        visited[u]=true;
+        LinkedList<Integer> queue = new LinkedList<>();
+        visited[u] = true;
         queue.add(u);
         Iterator<Integer> i;
         while (queue.size() != 0) {
