ed2edb82e2255d0e8b27a76544fd7ce007792679
powzx <69313768+powzx@users.noreply.github.com>
2021-10-06 22:04:26 +0800

    Add testing for invalid inputs with no recovery of schedule

--- src/test/java/seedu/duke/planner/PlannerTest.java
+++ src/test/java/seedu/duke/planner/PlannerTest.java
@@ -3,11 +3,12 @@ package seedu.duke.planner;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class PlannerTest {
 
     private String[] validEventArguments = new String[]{"Pop Quiz 3", "2021-10-26", "15:00", "15:15"};
+    private String[] invalidEventDateArguments = new String[]{"Something bad", "16/10/2020", "4pm", "7pm"};
+    private String[] invalidEventFormatArguments = new String[]{"Something worse", "2021-10-26", "07:00"};
     private String validList = "\n15:00 - 15:15 Pop Quiz 3";
 
     @Test
@@ -19,5 +20,23 @@ public class PlannerTest {
         Planner.clearEvent();
     }
 
-    
+    @Test
+    public void addEvent_invalidEventDateInput_eventNotAdded() {
+        Planner.clearEvent();
+        try {
+            Event invalidEvent = new Event(invalidEventDateArguments);
+        } catch (Exception e) {
+            assertEquals("Please provide a valid date and time!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void addEvent_invalidEventFormatInput_eventNotAdded() {
+        Planner.clearEvent();
+        try {
+            Event invalidEvent = new Event(invalidEventFormatArguments);
+        } catch (Exception e) {
+            assertEquals("Please double check the format of your input!", e.getMessage());
+        }
+    }
 }
