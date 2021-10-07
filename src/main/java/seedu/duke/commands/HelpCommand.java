2b8b00af5af2ca8d1ffbe78e357507178cc0dbe6
powzx <69313768+powzx@users.noreply.github.com>
2021-10-08 00:01:31 +0800

    Edit HelpCommand

--- src/main/java/seedu/duke/commands/HelpCommand.java
+++ src/main/java/seedu/duke/commands/HelpCommand.java
@@ -7,8 +7,10 @@ public class HelpCommand extends Command {
             + "2. view MODULE_CODE - View the module details\n"
             + "3. bus routes - Check for a NUS bus route from stop to another\n"
             + "4. planner add DESCRIPTION/DATE/START_TIME/END_TIME - Add an event to your schedule\n"
-            + "5. help - View this menu again\n"
-            + "6. bye - Exit Kolinux";
+            + "5. planner list DATE - Lists events on a certain date\n"
+            + "6. planner clear - Clears all events in your entire schedule\n"
+            + "7. help - View this menu again\n"
+            + "8. bye - Exit Kolinux";
 
     @Override
     public CommandResult executeCommand() {
