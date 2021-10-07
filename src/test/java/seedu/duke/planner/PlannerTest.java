b0b9d481ed82b25b592553b166f21c4c0949a33f
powzx <69313768+powzx@users.noreply.github.com>
2021-10-07 23:15:53 +0800

    Refactor PlannerTest

--- src/test/java/seedu/duke/planner/PlannerTest.java
+++ src/test/java/seedu/duke/planner/PlannerTest.java
@@ -1,31 +1,36 @@
 package seedu.duke.planner;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.KolinuxException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlannerTest {
 
-    private String[] validEventArguments = new String[]{"Pop Quiz 3", "2021-10-26", "15:00", "15:15"};
-    private String[] invalidEventDateArguments = new String[]{"Something bad", "16/10/2020", "4pm", "7pm"};
-    private String[] invalidEventFormatArguments = new String[]{"Something worse", "2021-10-26", "07:00"};
-    private String validList = "\n15:00 - 15:15 Pop Quiz 3";
+    private static final String[] VALID_EVENT_ARGUMENTS
+            = new String[]{"Pop Quiz 3", "2021-10-26", "15:00", "15:15"};
+    private static final String[] INVALID_EVENT_DATE_ARGUMENTS
+            = new String[]{"Something bad", "16/10/2020", "4pm", "7pm"};
+    private static final String[] INVALID_EVENT_FORMAT_ARGUMENTS
+            = new String[]{"Something worse", "2021-10-26", "07:00"};
+    private static final String VALID_LIST
+            = "\n15:00 - 15:15 Pop Quiz 3";
 
     @Test
-    public void addEvent_validEventInput_eventAdded() throws Exception {
+    public void addEvent_validEventInput_eventAdded() throws KolinuxException {
         Planner.clearEvent();
-        Event validEvent = new Event(validEventArguments);
+        Event validEvent = new Event(VALID_EVENT_ARGUMENTS);
         Planner.addEvent(validEvent);
-        assertEquals(validList, Planner.listEvents("2021-10-26"));
+        assertEquals(VALID_LIST, Planner.listEvents("2021-10-26"));
         Planner.clearEvent();
     }
 
     @Test
     public void addEvent_invalidEventDateInput_eventNotAdded() {
         try {
-            Event invalidEvent = new Event(invalidEventDateArguments);
+            Event invalidEvent = new Event(INVALID_EVENT_DATE_ARGUMENTS);
             Planner.addEvent(invalidEvent);
-        } catch (Exception e) {
+        } catch (KolinuxException e) {
             assertEquals("Please provide a valid date and time!", e.getMessage());
         }
     }
@@ -33,9 +38,9 @@ public class PlannerTest {
     @Test
     public void addEvent_invalidEventFormatInput_eventNotAdded() {
         try {
-            Event invalidEvent = new Event(invalidEventFormatArguments);
+            Event invalidEvent = new Event(INVALID_EVENT_FORMAT_ARGUMENTS);
             Planner.addEvent(invalidEvent);
-        } catch (Exception e) {
+        } catch (KolinuxException e) {
             assertEquals("Please double check the format of your input!", e.getMessage());
         }
     }
