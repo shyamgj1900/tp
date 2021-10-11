1bc4bee394f18abc7e0be74aab15c26b311803ef
giterator <pranavvenkatram@gmail.com>
2021-10-11 13:27:25 +0800

    ViewModuleInfoCommand: Add logging StoreModuleCommand: Add logging DeleteModuleCommand: Add logging

--- src/main/java/seedu/duke/commands/DeleteModuleCommand.java
+++ src/main/java/seedu/duke/commands/DeleteModuleCommand.java
@@ -2,6 +2,8 @@ package seedu.duke.commands;
 
 import seedu.duke.module.ModuleList;
 
+import java.util.logging.Level;
+
 public class DeleteModuleCommand extends Command {
     private String code;
 
@@ -12,6 +14,7 @@ public class DeleteModuleCommand extends Command {
     @Override
     public CommandResult executeCommand() {
         String message = ModuleList.deleteModuleByCode(code);
+        logger.log(Level.INFO, "User deleted module" + code + " from myModules list");
         return new CommandResult(message);
     }
 }
--- src/main/java/seedu/duke/commands/StoreModuleCommand.java
+++ src/main/java/seedu/duke/commands/StoreModuleCommand.java
@@ -2,6 +2,8 @@ package seedu.duke.commands;
 
 import seedu.duke.module.ModuleList;
 
+import java.util.logging.Level;
+
 public class StoreModuleCommand extends Command {
     private String code;
 
@@ -12,6 +14,7 @@ public class StoreModuleCommand extends Command {
     @Override
     public CommandResult executeCommand() {
         String message = ModuleList.storeModuleByCode(code);
+        logger.log(Level.INFO, "User stored module" + code + " in myModules list");
         return new CommandResult(message);
     }
 }
