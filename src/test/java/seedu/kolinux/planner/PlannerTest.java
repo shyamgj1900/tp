a0261b6b709239ba4da7277055122788f37186ee
powzx <69313768+powzx@users.noreply.github.com>
2021-10-14 00:46:09 +0800

    Add text ui and JUnit for wrong time order

--- src/test/java/seedu/kolinux/planner/PlannerTest.java
+++ src/test/java/seedu/kolinux/planner/PlannerTest.java
@@ -13,12 +13,16 @@ public class PlannerTest {
             = new String[]{"Something bad", "16/10/2020", "4pm", "7pm"};
     private static final String[] INVALID_EVENT_FORMAT_ARGUMENTS
             = new String[]{"Something worse", "2021-10-26", "07:00"};
+    private static final String[] WRONG_TIME_ORDER_ARGUMENTS
+            = new String[]{"Go back in time", "2021-04-06", "20:00", "16:00"};
     private static final String VALID_LIST
             = "\n15:00 - 15:15 Pop Quiz 3";
     private static final String DATETIME_ERROR =
             "Please provide a valid date and time! Format: yyyy-mm-dd";
     private static final String FORMAT_ERROR =
             "Please check the format of your input! Format: planner add DESCRIPTION/DATE/START_TIME/END_TIME";
+    private static final String TIME_ORDER_ERROR =
+            "Please check the format of the time! The end time is earlier than the start time...";
 
     @Test
     public void addEvent_validEventInput_eventAdded() throws KolinuxException {
@@ -46,6 +50,16 @@ public class PlannerTest {
             Planner.addEvent(invalidEvent);
         } catch (KolinuxException exception) {
             assertEquals(FORMAT_ERROR, exception.getMessage());
+        }
+    }
+
+    @Test
+    public void addEvent_wrongTimeOrderInput_eventNotAdded() {
+        try {
+            Event invalidEvent = new Event(WRONG_TIME_ORDER_ARGUMENTS);
+            Planner.addEvent(invalidEvent);
+        } catch (KolinuxException exception) {
+            assertEquals(TIME_ORDER_ERROR, exception.getMessage());
         }
     }
 }
