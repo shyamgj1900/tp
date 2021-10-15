6b3ac8ab05879a8c61cebef0528b4e6fc7e9c0bc
powzx <69313768+powzx@users.noreply.github.com>
2021-10-15 23:06:32 +0800

    Add JUnit for delete planner

--- src/test/java/seedu/kolinux/planner/PlannerTest.java
+++ src/test/java/seedu/kolinux/planner/PlannerTest.java
@@ -3,14 +3,18 @@ package seedu.kolinux.planner;
 import org.junit.jupiter.api.Test;
 import seedu.kolinux.exceptions.KolinuxException;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlannerTest {
 
     private Planner planner = new Planner();
 
-    private static final String[] VALID_EVENT_ARGUMENTS
-            = new String[]{"Pop Quiz 3", "2021-10-26", "1500", "1515"};
+    private static final String[][] VALID_EVENT_ARGUMENTS
+            = new String[][]{{"Pop Quiz 3", "2021-10-26", "1500", "1515"},
+                {"Pop Quiz 4", "2021-10-26", "1530", "2000"},
+                {"Pop Quiz 5", "2021-10-26", "2000", "2130"}};
     private static final String[] INVALID_EVENT_DATE_ARGUMENTS
             = new String[]{"Something bad", "16/10/2020", "4pm", "7pm"};
     private static final String[] INVALID_EVENT_FORMAT_ARGUMENTS
@@ -22,8 +26,11 @@ public class PlannerTest {
                 {"Do something", "2021-10-26", "1430", "1505"},
                 {"Do something", "2021-10-26", "1400", "1600"},
                 {"Do something", "2021-10-26", "1505", "1510"}};
-    private static final String VALID_LIST
+    private static final String VALID_LIST_1
             = "\n15:00 - 15:15 Pop Quiz 3";
+    private static final String VALID_LIST_2
+            = "\n15:00 - 15:15 Pop Quiz 3\n"
+                    + "20:00 - 21:30 Pop Quiz 5";
     private static final String DATETIME_ERROR
             = "Please provide a valid date and time!\n"
                     + "Date: yyyy-mm-dd\n"
@@ -38,9 +45,9 @@ public class PlannerTest {
     @Test
     public void addEvent_validEventInput_eventAdded() throws KolinuxException {
         planner.clearEvents();
-        Event validEvent = new Event(VALID_EVENT_ARGUMENTS);
+        Event validEvent = new Event(VALID_EVENT_ARGUMENTS[0]);
         planner.addEvent(validEvent);
-        assertEquals(VALID_LIST, planner.listEvents("2021-10-26", false));
+        assertEquals(VALID_LIST_1, planner.listEvents("2021-10-26", false));
         planner.clearEvents();
     }
 
@@ -77,7 +84,7 @@ public class PlannerTest {
     @Test
     public void addEvent_startTimeWithinAnotherEvent_eventNotAdded() throws KolinuxException {
         planner.clearEvents();
-        Event validEvent = new Event(VALID_EVENT_ARGUMENTS);
+        Event validEvent = new Event(VALID_EVENT_ARGUMENTS[0]);
         planner.addEvent(validEvent);
         try {
             Event invalidEvent = new Event(CONFLICTED_TIME_ARGUMENTS[0]);
@@ -91,7 +98,7 @@ public class PlannerTest {
     @Test
     public void addEvent_endTimeWithinAnotherEvent_eventNotAdded() throws KolinuxException {
         planner.clearEvents();
-        Event validEvent = new Event(VALID_EVENT_ARGUMENTS);
+        Event validEvent = new Event(VALID_EVENT_ARGUMENTS[0]);
         planner.addEvent(validEvent);
         try {
             Event invalidEvent = new Event(CONFLICTED_TIME_ARGUMENTS[1]);
@@ -105,7 +112,7 @@ public class PlannerTest {
     @Test
     public void addEvent_eventContainsAnotherEvent_eventNotAdded() throws KolinuxException {
         planner.clearEvents();
-        Event validEvent = new Event(VALID_EVENT_ARGUMENTS);
+        Event validEvent = new Event(VALID_EVENT_ARGUMENTS[0]);
         planner.addEvent(validEvent);
         try {
             Event invalidEvent = new Event(CONFLICTED_TIME_ARGUMENTS[2]);
@@ -119,7 +126,7 @@ public class PlannerTest {
     @Test
     public void addEvent_eventWithinAnotherEvent_eventNotAdded() throws KolinuxException {
         planner.clearEvents();
-        Event validEvent = new Event(VALID_EVENT_ARGUMENTS);
+        Event validEvent = new Event(VALID_EVENT_ARGUMENTS[0]);
         planner.addEvent(validEvent);
         try {
             Event invalidEvent = new Event(CONFLICTED_TIME_ARGUMENTS[3]);
@@ -127,6 +134,22 @@ public class PlannerTest {
         } catch (KolinuxException exception) {
             assertEquals(TIME_CONFLICT_ERROR, exception.getMessage());
         }
+        planner.clearEvents();
+    }
+
+    @Test
+    public void deleteEvent_validIdInput_eventDeleted() throws KolinuxException {
+        planner.clearEvents();
+        String idToBeDeleted = null;
+        for (String[] validInput : VALID_EVENT_ARGUMENTS) {
+            Event validEvent = new Event(validInput);
+            if (Arrays.equals(validInput, new String[]{"Pop Quiz 4", "2021-10-26", "1530", "2000"})) {
+                idToBeDeleted = validEvent.getId();
+            }
+            planner.addEvent(validEvent);
+        }
+        planner.deleteEvent(idToBeDeleted);
+        assertEquals(VALID_LIST_2, planner.listEvents("2021-10-26", false));
         planner.clearEvents();
     }
 }
