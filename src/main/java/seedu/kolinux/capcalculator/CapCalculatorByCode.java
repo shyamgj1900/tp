4442a96ab63298dbd1b60fa651f67d60a9d95262
NonRNP <69129667+NonRNP@users.noreply.github.com>
2021-10-16 10:36:21 +0800

    Handle invalid module code in cap calculator

--- src/main/java/seedu/kolinux/capcalculator/CapCalculatorByCode.java
+++ src/main/java/seedu/kolinux/capcalculator/CapCalculatorByCode.java
@@ -2,19 +2,25 @@ package seedu.kolinux.capcalculator;
 
 import seedu.kolinux.exceptions.KolinuxException;
 import seedu.kolinux.module.ModuleDb;
+import seedu.kolinux.module.ModuleDetails;
 
 public class CapCalculatorByCode extends CapCalculator {
 
-    private ModuleDb moduleDb = new ModuleDb().getPreInitModuleDb();
+    private ModuleDb moduleDb;
     
     public CapCalculatorByCode(String input) {
         super(input);
+        moduleDb = new ModuleDb().getPreInitModuleDb();
     }
     
     @Override
     protected int getMc(String module) throws KolinuxException {
         String[] moduleDescriptions = module.split("/");
         String moduleCode = moduleDescriptions[0];
+        ModuleDetails moduleInfo = moduleDb.getModuleInfo(moduleCode);
+        if (moduleInfo == null) {
+            throw new KolinuxException("Invalid module code found");
+        }
         String moduleCredit = moduleDb.getModuleInfo(moduleCode).getModuleCredit();
         return Integer.parseInt(moduleCredit);
     }
