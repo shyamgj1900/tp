8b338155ca0f40042ab8d96978aa22c72cbc8a93
powzx <69313768+powzx@users.noreply.github.com>
2021-10-18 16:33:05 +0800

    Implement methods for PlannerLister

--- src/main/java/seedu/kolinux/planner/PlannerLister.java
+++ src/main/java/seedu/kolinux/planner/PlannerLister.java
@@ -16,25 +16,20 @@ public class PlannerLister {
     private String date;
 
     private static final String INVALID_DATE_MESSAGE = "Please provide a valid date. Format: yyyy-mm-dd";
+    private static final String FATAL_ERROR = "Fatal error occurred, please restart Kolinux.";
 
     private ArrayList<Lesson> lessonsOnDate = new ArrayList<>();
+    private ArrayList<Event> convertedLessonsOnDate = new ArrayList<>();
 
     public PlannerLister(String date) throws KolinuxException {
-        try {
-            LocalDate.parse(date);
-        } catch (DateTimeParseException exception) {
-            throw new KolinuxException(INVALID_DATE_MESSAGE);
-        }
         this.date = date;
         getLessonsOnDate();
+        convertLessonListToEventList();
     }
 
-    /**
-    private void convertLessonToEvent(Lesson lesson) {
-        String[] arguments = new String[4];
-        arguments[0] = lesson.getModuleCode() + " " + lesson.getLessonType();
-        arguments[1] = date;
-    }*/
+    public ArrayList<Event> getConvertedLessonsOnDate() {
+        return convertedLessonsOnDate;
+    }
 
     private void getLessonsOnDate() throws KolinuxException {
         int dayAsInteger;
@@ -48,6 +43,29 @@ public class PlannerLister {
         lessonsOnDate = (ArrayList<Lesson>) Timetable.lessonStorage
                 .stream()
                 .filter(lesson -> day.equalsIgnoreCase(lesson.getDay()))
+                .collect(Collectors.toList());
+    }
+
+    private Event convertLessonToEvent(Lesson lesson) {
+        String[] arguments = new String[4];
+        arguments[0] = lesson.getModuleCode() + " " + lesson.getLessonType();
+        arguments[1] = date;
+        arguments[2] = lesson.getStartTime();
+        arguments[3] = lesson.getEndTime();
+        Event event = null;
+
+        try {
+            event = new Event(arguments);
+        } catch (KolinuxException exception) {
+            System.out.println(FATAL_ERROR);
+        }
+        return event;
+    }
+
+    private void convertLessonListToEventList() {
+        convertedLessonsOnDate = (ArrayList<Event>) lessonsOnDate
+                .stream()
+                .map(lesson -> convertLessonToEvent(lesson))
                 .collect(Collectors.toList());
     }
 }
