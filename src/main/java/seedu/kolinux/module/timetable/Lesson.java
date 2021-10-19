358580c2e6f47c4eebfd8e4afe15d78fb386d52a
saravanamani1999 <66838856+saravanamani1999@users.noreply.github.com>
2021-10-20 02:49:35 +0800

    Change checkstyle to allow indentation of 8 spaces for array initialization

--- src/main/java/seedu/kolinux/module/timetable/Lesson.java
+++ src/main/java/seedu/kolinux/module/timetable/Lesson.java
@@ -16,7 +16,7 @@ public class Lesson {
     protected int endTimeIndex;
     protected int dayIndex;
     public static String [] schoolHours = new String [] {"0600", "0700", "0800", "0900", "1000", "1100",
-        "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100"};
+            "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100"};
     public static String[] days = new String[] {"monday", "tuesday", "wednesday", "thursday", "friday"};
 
     public Lesson(String[] parsedArguments) throws KolinuxException {
