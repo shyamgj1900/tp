ea4409f5f7ba47f3fe9e6d9f4352911f4b39700b
Shyam <e0544484@u.nus.edu>
2021-10-20 12:28:31 +0800

    Fix checkstyle error

--- src/main/java/seedu/kolinux/routes/Location.java
+++ src/main/java/seedu/kolinux/routes/Location.java
@@ -48,34 +48,34 @@ public class Location {
     public int getStopNumberAOne(String command) {
         assert command != null;
         switch (command.trim().toLowerCase()) {
-            case "kr bus terminal":
-                return 0;
-            case "lt13":
-                return 1;
-            case "as 5":
-                return 2;
-            case "com 2":
-                return 3;
-            case "biz 2":
-                return 4;
-            case "opp tcoms":
-                return 5;
-            case "pgp":
-                return 6;
-            case "kr mrt":
-                return 7;
-            case "lt27":
-                return 8;
-            case "uhall":
-                return 9;
-            case "opp uhc":
-                return 10;
-            case "yih":
-                return 11;
-            case "clb":
-                return 12;
-            default:
-                return -1;
+        case "kr bus terminal":
+            return 0;
+        case "lt13":
+            return 1;
+        case "as 5":
+            return 2;
+        case "com 2":
+            return 3;
+        case "biz 2":
+            return 4;
+        case "opp tcoms":
+            return 5;
+        case "pgp":
+            return 6;
+        case "kr mrt":
+            return 7;
+        case "lt27":
+            return 8;
+        case "uhall":
+            return 9;
+        case "opp uhc":
+            return 10;
+        case "yih":
+            return 11;
+        case "clb":
+            return 12;
+        default:
+            return -1;
         }
     }
 
@@ -89,36 +89,36 @@ public class Location {
     public int getStopNumberATwo(String command) {
         assert command != null;
         switch (command.trim().toLowerCase()) {
-            case "kr bus terminal":
-                return 0;
-            case "it":
-                return 1;
-            case "opp yih":
-                return 2;
-            case "museum":
-                return 3;
-            case "uhc":
-                return 4;
-            case "opp uhall":
-                return 5;
-            case "s 17":
-                return 6;
-            case "opp kr mrt":
-                return 7;
-            case "pgpr":
-                return 8;
-            case "tcoms":
-                return 9;
-            case "opp hssml":
-                return 10;
-            case "opp nuss":
-                return 11;
-            case "com 2":
-                return 12;
-            case "ventus":
-                return 13;
-            default:
-                return -1;
+        case "kr bus terminal":
+            return 0;
+        case "it":
+            return 1;
+        case "opp yih":
+            return 2;
+        case "museum":
+            return 3;
+        case "uhc":
+            return 4;
+        case "opp uhall":
+            return 5;
+        case "s 17":
+            return 6;
+        case "opp kr mrt":
+            return 7;
+        case "pgpr":
+            return 8;
+        case "tcoms":
+            return 9;
+        case "opp hssml":
+            return 10;
+        case "opp nuss":
+            return 11;
+        case "com 2":
+            return 12;
+        case "ventus":
+            return 13;
+        default:
+            return -1;
         }
     }
 
@@ -132,34 +132,34 @@ public class Location {
     public int getStopNumberDOne(String command) {
         assert command != null;
         switch (command.trim().toLowerCase()) {
-            case "opp hssml":
-                return 0;
-            case "opp nuss":
-                return 1;
-            case "com 2":
-                return 2;
-            case "ventus":
-                return 3;
-            case "it":
-                return 4;
-            case "opp yih":
-                return 5;
-            case "museum":
-                return 6;
-            case "utown":
-                return 7;
-            case "yih":
-                return 8;
-            case "clb":
-                return 9;
-            case "lt13":
-                return 10;
-            case "as 5":
-                return 11;
-            case "biz 2":
-                return 12;
-            default:
-                return -1;
+        case "opp hssml":
+            return 0;
+        case "opp nuss":
+            return 1;
+        case "com 2":
+            return 2;
+        case "ventus":
+            return 3;
+        case "it":
+            return 4;
+        case "opp yih":
+            return 5;
+        case "museum":
+            return 6;
+        case "utown":
+            return 7;
+        case "yih":
+            return 8;
+        case "clb":
+            return 9;
+        case "lt13":
+            return 10;
+        case "as 5":
+            return 11;
+        case "biz 2":
+            return 12;
+        default:
+            return -1;
         }
     }
 
@@ -173,32 +173,32 @@ public class Location {
     public int getStopNumberDTwo(String command) {
         assert command != null;
         switch (command.trim().toLowerCase()) {
-            case "pgp":
-                return 0;
-            case "kr mrt":
-                return 1;
-            case "lt27":
-                return 2;
-            case "uhall":
-                return 3;
-            case "opp uhc":
-                return 4;
-            case "museum":
-                return 5;
-            case "utown":
-                return 6;
-            case "uhc":
-                return 7;
-            case "opp uhall":
-                return 8;
-            case "s 17":
-                return 9;
-            case "opp kr mrt":
-                return 10;
-            case "pgpr":
-                return 11;
-            default:
-                return -1;
+        case "pgp":
+            return 0;
+        case "kr mrt":
+            return 1;
+        case "lt27":
+            return 2;
+        case "uhall":
+            return 3;
+        case "opp uhc":
+            return 4;
+        case "museum":
+            return 5;
+        case "utown":
+            return 6;
+        case "uhc":
+            return 7;
+        case "opp uhall":
+            return 8;
+        case "s 17":
+            return 9;
+        case "opp kr mrt":
+            return 10;
+        case "pgpr":
+            return 11;
+        default:
+            return -1;
         }
     }
 
@@ -212,22 +212,22 @@ public class Location {
     public int getStopNumberE(String command) {
         assert command != null;
         switch (command.trim().toLowerCase()) {
-            case "kent vale":
-                return 0;
-            case "ea":
-                return 1;
-            case "sde 3":
-                return 2;
-            case "it":
-                return 3;
-            case "opp yih":
-                return 4;
-            case "utown":
-                return 5;
-            case "raffles hall":
-                return 6;
-            default:
-                return -1;
+        case "kent vale":
+            return 0;
+        case "ea":
+            return 1;
+        case "sde 3":
+            return 2;
+        case "it":
+            return 3;
+        case "opp yih":
+            return 4;
+        case "utown":
+            return 5;
+        case "raffles hall":
+            return 6;
+        default:
+            return -1;
         }
     }
 
@@ -241,40 +241,40 @@ public class Location {
     public int getStopNumberK(String command) {
         assert command != null;
         switch (command.trim().toLowerCase()) {
-            case "pgp":
-                return 0;
-            case "kr mrt":
-                return 1;
-            case "lt27":
-                return 2;
-            case "uhall":
-                return 3;
-            case "opp uhc":
-                return 4;
-            case "yih":
-                return 5;
-            case "clb":
-                return 6;
-            case "opp sde 3":
-                return 7;
-            case "japanese pri school":
-                return 8;
-            case "kent vale":
-                return 9;
-            case "museum":
-                return 10;
-            case "uhc":
-                return 11;
-            case "opp uhall":
-                return 12;
-            case "s 17":
-                return 13;
-            case "opp kr mrt":
-                return 14;
-            case "pgpr":
-                return 15;
-            default:
-                return -1;
+        case "pgp":
+            return 0;
+        case "kr mrt":
+            return 1;
+        case "lt27":
+            return 2;
+        case "uhall":
+            return 3;
+        case "opp uhc":
+            return 4;
+        case "yih":
+            return 5;
+        case "clb":
+            return 6;
+        case "opp sde 3":
+            return 7;
+        case "japanese pri school":
+            return 8;
+        case "kent vale":
+            return 9;
+        case "museum":
+            return 10;
+        case "uhc":
+            return 11;
+        case "opp uhall":
+            return 12;
+        case "s 17":
+            return 13;
+        case "opp kr mrt":
+            return 14;
+        case "pgpr":
+            return 15;
+        default:
+            return -1;
         }
     }
 }
