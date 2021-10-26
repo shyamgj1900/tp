53c7091907f277e602bd709ab5665475f1065b74
saravanamani1999 <66838856+saravanamani1999@users.noreply.github.com>
2021-10-26 17:36:59 +0800

    fix checkstyle error

--- src/main/java/seedu/kolinux/module/ModuleList.java
+++ src/main/java/seedu/kolinux/module/ModuleList.java
@@ -126,7 +126,7 @@ public class ModuleList {
             if (preparationHours > 0) {
                 System.out.println("Preparation: " + preparationHours + " hours");
             }
-            if (preparationHours < 0 && projectHours < 0 && labHours < 0 && tutorialHours < 0 && lectureHours < 0 ) {
+            if (preparationHours < 0 && projectHours < 0 && labHours < 0 && tutorialHours < 0 && lectureHours < 0) {
                 System.out.println("No workload information available for " + code);
             }
             String examDate = module.getDate();
